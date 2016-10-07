506abe7e1ecca066aaf2e35e7a406bc707ae36f3
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-07 17:36:52 +0200

    device.mk: improve some formatting

--- device.mk
+++ device.mk
@@ -1,18 +1,19 @@
-#
+# 
 # Copyright (C) 2015 The CyanogenMod Project
-#
+# Copyright (C) 2016 halogenOS
+# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
+# 
 #      http://www.apache.org/licenses/LICENSE-2.0
-#
+# 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+# 
 
 $(call inherit-product-if-exists, vendor/oneplus/oneplus2/oneplus2-vendor.mk)
 
@@ -242,7 +243,8 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
-PRODUCT_PACKAGES += telephony-ext
+# Telephony
+PRODUCT_PACKAGES  += telephony-ext
 PRODUCT_BOOT_JARS += telephony-ext
 
 # Sensors
