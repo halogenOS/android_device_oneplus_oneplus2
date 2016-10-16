4297c3c4219ce6d2e10d927efc0148d086a442d0
Hunter <hbashton13@gmail.com>
2016-10-16 13:27:37 -0500

    Add SU binary
    it will only work with phh zip :)

--- device.mk
+++ device.mk
@@ -288,10 +288,16 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
-
+	
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+
+PRODUCT_COPY_FILES += \
+    device/oneplus/oneplus2/su-arm:system/xbin/su
+
+# SU Support
+SUPERUSER_EMBEDDED := true
 
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
