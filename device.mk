5b61f7e0d0e1a40cd026ecc4bc1fe19beea518cc
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-24 13:27:23 +0100

    Remove doze stuff
    Change-Id: If9f5aaafba5b6dba5945b36b4205ef013effe3f5

--- device.mk
+++ device.mk
@@ -137,10 +137,6 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
