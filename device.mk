078b379d83e4aa13a536ca34bfd08647a1bd00ee
Simao Gomes Viana <xdevs23@outlook.com>
2017-02-16 16:11:47 +0100

    Qualcomm BT aptX & aptX HD codecs in N
    Change-Id: I16b84aa42a6f6a85efed298dc2ec1c499a320fa8

--- device.mk
+++ device.mk
@@ -290,5 +290,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/wifi/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Qualcomm BT aptX & aptX HD codecs in N
+$(call inherit-product, vendor/qcom/binaries/bluetooth/bluetooth-vendor.mk)
+
 # Inherit from oppo-common
 $(call inherit-product, device/oppo/common/common.mk)
