2ecbeed37edb0739273e45147dbb8b3340c42cd7
Simao Gomes Viana <xdevs23@outlook.com>
2017-02-24 16:38:10 +0100

    BoardConfig: rename defconfig
    Change-Id: I47c3612b723dc9d913a8c852edeacae1254ce393

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8994
-TARGET_KERNEL_CONFIG := cm_oneplus2_defconfig
+TARGET_KERNEL_CONFIG := oneplus2_defconfig
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
