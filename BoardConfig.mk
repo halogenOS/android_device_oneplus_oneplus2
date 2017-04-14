b52bd8271716e00d9c9765a568657227aa8e4dcb
Simao Gomes Viana <xdevs23@outlook.com>
2017-04-14 13:16:41 +0200

    BoardConfig: add back lpm_levels.sleep_disabled=1
    Change-Id: I6be766c31e6067391a7e313ac276e93eb0c7eec3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,7 @@ KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/a
 TARGET_TOOLCHAIN_ROOT := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(strip $(HOST_OS))-x86/aarch64/$(TARGET_KERNEL_CROSS_COMPILE_PREFIX)4.9/bin
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=29 msm_rtb.filter=0x37 ehci-hcd.park=3 boot_cpus=0-7 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=29 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-7 androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
