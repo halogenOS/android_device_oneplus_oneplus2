88326d9bc5677b5bd5a5c53d85dea02461cf3ed8
Simao Gomes Viana <xdevs23@outlook.com>
2017-04-15 18:36:48 +0200

    Welcome to the HAL3 party!
    Change-Id: Idec84121d1aaf70983ed152fdcf54c50eb9773a7
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- Android.mk
+++ Android.mk
@@ -17,6 +17,7 @@ MM_V4L2_DRIVER_LIST += msm8992
 ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
   ifneq ($(strip $(USE_CAMERA_STUB)),true)
     ifneq ($(BUILD_TINY_ANDROID),true)
+      $(info Welcome to the HAL3 party!)
       include $(call all-subdir-makefiles)
     endif
   endif
