af7f7f5f7a03d99a1417ea2a78e5104ec02c4790
Simao Gomes Viana <xdevs23@outlook.com>
2017-06-12 22:10:40 +0200

    Add charger sepolicy dir to board sepolicy dirs
    Change-Id: I64b24236c4b34c58f38c47e8c64ea0b64e52664d
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -253,7 +253,9 @@ HWUI_COMPILE_FOR_PERF := true
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    $(PLATFORM_PATH)/sepolicy \
+    $(PLATFORM_PATH)/charger/sepolicy
 
 # Time services
 BOARD_USES_QC_TIME_SERVICES := true
