e36e1f0b6e66f878682674a334ae6b9a696af939
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    init: Update for O
    Change-Id: Ia5b075b3a628a7af5ca72fae611237b7a4e2bb61

--- init/Android.mk
+++ init/Android.mk
@@ -9,6 +9,7 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -O3
 LOCAL_SRC_FILES := init_oneplus2.cpp
 LOCAL_MODULE := libinit_oneplus2
+LOCAL_STATIC_LIBRARIES += libbase
 
 include $(BUILD_STATIC_LIBRARY)
 
