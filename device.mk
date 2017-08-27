0a9a1aee8f5e215df5d02dd9f844aef265fc6cc0
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-27 12:58:52 +0200

    device.mk: don't build charger for now
    Change-Id: I73703e61c69ee306a0a6a74693a8d7da8071da8c

--- device.mk
+++ device.mk
@@ -122,11 +122,11 @@ PRODUCT_PACKAGES += \
     services-ext
 
 # Charger
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    cm_charger_res_images \
-    font_log.png \
-    libhealthd.cm
+#PRODUCT_PACKAGES += \
+#    charger_res_images \
+#    cm_charger_res_images \
+#    font_log.png \
+#    libhealthd.cm
 
 # Display
 PRODUCT_PACKAGES += \
