5bf826b8cdaf39741ef73f22bf6b88cdfb77f604
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-29 14:35:24 +0200

    camera: add O dependencies
    Some headers in the O tree require these.
    
    Change-Id: I69b723a17fa3639f4ca9915fd47435fa20682dac

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -57,7 +57,9 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../mm-image-codec/qomx_core \
         $(LOCAL_PATH)/util \
         hardware/qcom/media-caf/msm8994/libstagefrighthw \
-        device/qcom/common/power
+        device/qcom/common/power \
+        $(call include-path-for, android.hidl.token@1.0-utils) \
+        $(call include-path-for, android.hardware.graphics.bufferqueue@1.0)
 
 #HAL 1.0 Include paths
 LOCAL_C_INCLUDES += \
@@ -76,6 +78,8 @@ LOCAL_C_INCLUDES += \
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqdutils
+LOCAL_SHARED_LIBRARIES += android.hidl.token@1.0-utils
+LOCAL_SHARED_LIBRARIES += android.hardware.graphics.bufferqueue@1.0
 
 LOCAL_CLANG := false
 LOCAL_MODULE_RELATIVE_PATH := hw
