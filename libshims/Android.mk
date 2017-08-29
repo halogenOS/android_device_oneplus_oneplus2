9d632d0fa05dc0d820b57f9c36c27aaaea88a4f2
Simao Gomes Viana <xdevs23@outlook.com>
2017-08-29 12:07:05 +0200

    Partially revert "oneplus2: update libshims for oreo"
    Change-Id: I46c7cc6ec1de3e714735fd9a4eaa99076f470bd5

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -54,17 +54,6 @@ LOCAL_C_INCLUDES += \
 	system/media/camera/include \
 	system/media/private/camera/include
 
-LOCAL_MODULE:= libshim_ims-camera
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := camera.cpp
-LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_32_BIT_ONLY := true
-LOCAL_SHARED_LIBRARIES := libgui libui libsensor
+LOCAL_MODULE:= libshim_camera
 
 include $(BUILD_SHARED_LIBRARY)
