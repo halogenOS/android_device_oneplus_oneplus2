1a63941f93fe7e690f75e0513a51f81d91a059cc
Shreesha <shreeshu10@gmail.com>
2017-09-01 20:25:20 -0400

    op2: disable oppo_common

--- device.mk
+++ device.mk
@@ -340,4 +340,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
+#$(call inherit-product, device/oppo/common/common.mk)
