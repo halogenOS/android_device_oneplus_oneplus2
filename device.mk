e74d9e43b7a87c2632cc943cb96d583e80f51419
Shreesha <shreeshu10@gmail.com>
2017-09-04 21:50:38 -0400

    finally!
    Change-Id: I7e2858c3567d8749c747b91442ece943f9ea17df
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- device.mk
+++ device.mk
@@ -332,9 +332,6 @@ PRODUCT_PACKAGES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    ipacm \
-    ipacm-diag \
-    IPACM_cfg.xml \
     hostapd \
     libwpa_client \
     wificond \
