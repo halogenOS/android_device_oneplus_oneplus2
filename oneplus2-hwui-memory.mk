b4422f75cad37fd2e86c54e4225e972737ee8bb9
Simao Gomes Viana <xdevs23@outlook.com>
2017-09-18 14:55:23 +0200

    Increase some hwui stuff
    Change-Id: Ife49349c627d633043ac898a4f3be9e0a0be331d

--- oneplus2-hwui-memory.mk
+++ oneplus2-hwui-memory.mk
@@ -1,7 +1,7 @@
 #
 # Copyright (C) 2015 The CyanogenMod Project
 # Copyright (C) 2015-2016 OnePlus Ltd.
-# Copyright (C) 2016 halogenOS
+# Copyright (C) 2016-2017 The halogenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,14 +20,14 @@
 
 # These are taken from OOS3
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.hwui.texture_cache_size=96 \
-	ro.hwui.layer_cache_size=64 \
-	ro.hwui.r_buffer_cache_size=12 \
-	ro.hwui.path_cache_size=39 \
-	ro.hwui.gradient_cache_size=1 \
-	ro.hwui.drop_shadow_cache_size=7 \
+	ro.hwui.texture_cache_size=112 \
+	ro.hwui.layer_cache_size=72 \
+	ro.hwui.r_buffer_cache_size=16 \
+	ro.hwui.path_cache_size=44 \
+	ro.hwui.gradient_cache_size=2 \
+	ro.hwui.drop_shadow_cache_size=9 \
 	ro.hwui.texture_cache_flushrate=0.4 \
-	ro.hwui.text_small_cache_width=2048 \
-	ro.hwui.text_small_cache_height=2048 \
-	ro.hwui.text_large_cache_width=3072 \
-	ro.hwui.text_large_cache_height=2048
+	ro.hwui.text_small_cache_width=3072 \
+	ro.hwui.text_small_cache_height=3072 \
+	ro.hwui.text_large_cache_width=4096 \
+	ro.hwui.text_large_cache_height=3072
