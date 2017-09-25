adaf696b02437c8dd8353c4a7641d95646332436
Shreesha <shreeshu10@gmail.com>
2017-09-25 16:51:30 +0530

    op2: camera: Initial OSS HAL port
    Full credit to Simao (@xdevs23) for the bring up on nougat
    
    Change-Id: Ibbceb9e11c23e62cec41505d2c2c419698268e45
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- device.mk
+++ device.mk
@@ -123,13 +123,17 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    libshim_atomic \
     camera.msm8994 \
+    mm-qcamera-app \
     libshim_camera \
     libshim_ims-camera \
     sensors.hal.tof \
-    android.hardware.camera.provider@2.4-impl-op2 \
-    libshim_atomic \
-    libop2_cam
+    libmmcamera_interface \
+    libmmjpeg_interface \
+    libqomx_core
 
 # DRM
 PRODUCT_PACKAGES += \
