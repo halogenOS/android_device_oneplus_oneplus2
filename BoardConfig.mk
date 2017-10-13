cc35e7e826afd9d632b5f3d4e2debd207f018fb6
Shreesha <shreeshu10@gmail.com>
2017-10-13 17:23:35 -0400

    op2: Correct DT2W node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,7 +115,7 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Cpusets
 ENABLE_CPUSETS := true
