c1eca29caf71e36aaabe3b2f645ca2f4deac36f2
Shreesha <shreeshu10@gmail.com>
2017-10-13 17:23:35 -0400

    op2: Correct DT2W node
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,7 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap"
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
 TARGET_LDPRELOAD := libNimsWrap.so
