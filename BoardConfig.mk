af3c24fe97bb2ee1635d2dec38c24b577820ede0
Shreesha <shreeshu10@gmail.com>
2017-10-16 17:59:42 -0400

    op2: Disable cmhw

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,7 @@ BOARD_BUILD_OP2_CAMERA := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CM Hardware
-BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
+#BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Cpusets
