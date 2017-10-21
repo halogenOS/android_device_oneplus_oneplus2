52874d4c851f9aa3c8f16243097ffc24423c4c76
Simao Gomes Viana <xdevs23@outlook.com>
2017-10-21 18:15:31 +0200

    BoardConfig: finally...
    Change-Id: I3315b3e93738be8d51ea898b38276c607c263d33

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8994
 TARGET_KERNEL_CONFIG := oneplus2_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
