43c65bf1d3ed986aa29fcfc0f4a80696df07229f
Simao Gomes Viana <xdevs23@outlook.com>
2017-10-28 14:01:40 +0200

    init: depend on libselinux_defaults
    In file included from device/oneplus/oneplus2/init/init_oneplus2.cpp:35:
    system/core/init/util.h:29:10: fatal error: 'selinux/label.h' file not found
    
    Change-Id: Id27b13647465e03a9131e385876e0cf655baccca

--- init/Android.mk
+++ init/Android.mk
@@ -3,12 +3,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := system/core/init external/selinux/libselinux/include
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_oneplus2.cpp
 LOCAL_MODULE := libinit_oneplus2
 
 LOCAL_STATIC_LIBRARIES := \
-     libbase
+     libbase \
+     libselinux_defaults
 
 include $(BUILD_STATIC_LIBRARY)
