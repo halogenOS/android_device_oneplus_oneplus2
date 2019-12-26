14bd6691294b1a17e0f29030e8ce8db98a81d547
Simao Gomes Viana <xdevs23@outlook.com>
2019-12-27 00:32:15 +0100

    halogenify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2019 The halogenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +15,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_oneplus2.mk
+    $(LOCAL_DIR)/aosp_oneplus2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_oneplus2-user \
-    lineage_oneplus2-userdebug \
-    lineage_oneplus2-eng
+    aosp_oneplus2-user \
+    aosp_oneplus2-userdebug \
+    aosp_oneplus2-eng
--- aosp_oneplus2.mk
+++ aosp_oneplus2.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk
 $(call inherit-product, device/oneplus/oneplus2/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, $(CUSTOM_VENDOR_DIR)/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_oneplus2
+PRODUCT_NAME := aosp_oneplus2
 PRODUCT_DEVICE := oneplus2
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
